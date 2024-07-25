212b0219354c6c4a179b83be8da1e06e9b2c2b7a
yasyasench <yasyasench@gmail.com>
2024-07-25 19:13:14 +0200

    upgraded second task

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,13 @@
-function calcAverageCalories(days){
-  let totalCalories=0;
-  for (day of days){
+function calcAverageCalories(days) {
+  if (days.length === 0) {
+    return 0;
+  }
+
+  let totalCalories = 0;
+  for (const day of days) {
     totalCalories += day.calories;
   }
-  return totalCalories/days.length;
+  return totalCalories / days.length;
 }
 
 
